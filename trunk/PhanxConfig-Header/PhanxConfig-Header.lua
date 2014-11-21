409bb1e6ecdb427d8646c1215ea4aecf9cadab40
Phanx-28751 <Phanx-28751@5d13539c-25a5-4bc8-b52a-5474e3ee8778>
2014-11-21 07:44:51 +0000

    Gotta keep using SVN apparently, since the Curse packager and Git are both steaming piles of shit when it comes to supporting externals.

--- trunk/PhanxConfig-Header/PhanxConfig-Header.lua
+++ trunk/PhanxConfig-Header/PhanxConfig-Header.lua
@@ -2,12 +2,11 @@
 	PhanxConfig-Header
 	Simple options panel header generator.
 	Requires LibStub.
-	https://github.com/phanx/PhanxConfigWidgets
 	Copyright (c) 2009-2014 Phanx. All rights reserved.
-	See the accompanying README and LICENSE files for more information.
+	https://github.com/phanx/PhanxConfigWidgets
 ----------------------------------------------------------------------]]
 
-local MINOR_VERSION = tonumber(strmatch("$Revision$", "%d+"))
+local MINOR_VERSION = 172
 
 local lib, oldminor = LibStub:NewLibrary("PhanxConfig-Header", MINOR_VERSION)
 if not lib then return end
